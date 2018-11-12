5f07432b90e23d1770edcea27a49ff5d15af3303
cary wang <carywang@carys-MacBook.local>
2018-11-12 23:12:23 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/users/cool', function(req, res, next) {
+router.get('/cool', function(req, res, next) {
   res.send("You're so cool");
 });
 
